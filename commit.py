7d3de73a97d5362796d41603b49818258c54108e
luciodmd <luciodavid93@gmail.com>
2018-06-30 19:10:08 -0400

    Started client

---
+++ commit.py
@@ -0,0 +1,37 @@
+import socket, threading, os
+
+class Commit():
+    files = None
+    host = None
+    port = 8000
+
+    def __init__(self, files, host):
+        if files != []:
+            self.files = files
+            if host != None:
+                self.host = host
+            for file in self.files:
+                self.sendFile(file)
+        else:
+            print('Can not commit nothing!')
+            sys.exit(1)
+    
+    def printFiles(self):
+        for i in self.files:
+            print(i)
+    
+    def sendFile(self, file):
+        commitSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        commitSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if self.host == None:
+            self.host = socket.gethostname()
+        commitSocket.connect((self.host, self.port))
+
+        with open(file, 'rb') as f:
+            while True:
+                data = f.read(1024)
+                commitSocket.send(data)
+                if not data:
+                    break
+            f.close
+        commitSocket.close()
---
+++ legitClient.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+from commit import Commit
+
+def show_usage():
+    print('usage:')
+    exit()
+
+def main(argv):
+    if argv[1] == 'commit':
+        if argv[2:] == []:
+            print ('Empty')
+            show_usage()
+        print('commit')
+        c = Commit(argv[2:], None)
+        c.printFiles()
+    elif argv[1] == 'update':
+        print('update')
+    elif argv[1] == 'checkout':
+        print('checkout')
+    else:
+        show_usage()
+
+if __name__ == '__main__':
+    main(sys.argv)
